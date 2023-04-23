1061a276b869499b196d09972df09268907204b0
Parth971 <desaiparth971@gmail.com>
2023-04-23 15:06:57 +0530

    Built: Create basic django app
    - Create accounts and dashboard app
    - Override User model in accounts app
    - Add home page in dashboard app
    - Add .gitignore file
    - Add sample_env file

---
+++ accounts/models.py
@@ -0,0 +1,19 @@
+import uuid
+
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from accounts.managers import UserManager
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, null=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
