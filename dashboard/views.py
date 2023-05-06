b34bd4b67d4ba16d7b0b3e583c14b32abced51cb
Parth971 <desaiparth971@gmail.com>
2023-05-06 22:01:36 +0530

    Display data in dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,16 @@
 from django.views.generic import TemplateView
 
+from blog.models import Post
+from dashboard.models import Skill
+from project.models import Project
+
 
 class HomeView(TemplateView):
     template_name = 'dashboard/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        context['skills'] = Skill.objects.all()
+        context['projects'] = Project.objects.all()
+        context['blogs'] = Post.objects.all()[:6]
+        return context
