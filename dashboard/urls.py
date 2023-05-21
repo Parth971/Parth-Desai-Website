a9880fc95a8b2b644e506a4168e052846d397bc4
Parth971 <desaiparth971@gmail.com>
2023-05-21 22:39:39 +0530

    Add missing links to main page

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from dashboard.views import HomeView
+from dashboard.views import HomeView, ServicesView
 
 urlpatterns = [
     path("", HomeView.as_view(), name='home'),
+    path("services/", ServicesView.as_view(), name='services'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -11,6 +11,21 @@ class HomeView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
         context['skills'] = Skill.objects.all()
-        context['projects'] = Project.objects.all()
-        context['blogs'] = Post.objects.all()[:6]
+        context['projects'] = Project.objects.all()[:4]
+        context['blogs'] = Post.objects.all()[:3]
+        context['social_media'] = {
+            'facebook': '',
+            'twiter': '',
+            'linkedin': '',
+            'google': '',
+            'github': '',
+            'instagram': '',
+            'upwork': '',
+            'freelancer': '',
+        }
+        context['cv_url'] = self.request.build_absolute_uri('/media/dashboard/CV2023.pdf')
         return context
+
+
+class ServicesView(TemplateView):
+    template_name = 'dashboard/services.html'
